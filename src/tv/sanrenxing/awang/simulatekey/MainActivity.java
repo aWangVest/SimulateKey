0a50933316712a76f51d1558223219a5d2b8d559
aWang <aWang@sanrenxing.tv>
2015-03-11 13:30:21 +0800

    提取悬浮窗操作的公共方法到SimulateKeyWinOpt类,另外增加悬浮窗显示与隐藏的控制

--- src/tv/sanrenxing/awang/simulatekey/MainActivity.java
+++ src/tv/sanrenxing/awang/simulatekey/MainActivity.java
@@ -2,7 +2,6 @@ package tv.sanrenxing.awang.simulatekey;
 
 import tv.sanrenxing.awang.utils.SimulateKeyUtils;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,30 +18,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		showFloatWindow();
-	}
-
-	/**
-	 * 显示悬浮窗
-	 */
-	protected void showFloatWindow() {
-		Intent intent = new Intent(SimulateKeyService.NAME);
-		// 第一次启动的时候，可以不带参数 //
-		intent.putExtra(SimulateKeyService.KEY_HAS_EXTRA, true);
-		intent.putExtra(SimulateKeyService.KEY_DO_ACTION,
-				SimulateKeyService.DO_ACTION_SHOWWINDOW);
-		startService(intent);
-	}
-
-	/**
-	 * 隐藏悬浮窗
-	 */
-	protected void hiddenFloatWindow() {
-		Intent intent = new Intent(SimulateKeyService.NAME);
-		intent.putExtra(SimulateKeyService.KEY_HAS_EXTRA, true);
-		intent.putExtra(SimulateKeyService.KEY_DO_ACTION,
-				SimulateKeyService.DO_ACTION_HIDDENWINDOW);
-		startService(intent);
+		SimulateKeyWinOpt.showFloatWindow(getApplicationContext());
 	}
 
 	/**
@@ -66,17 +42,33 @@ public class MainActivity extends Activity {
 	}
 
 	/**
+	 * 显示悬浮窗
+	 * 
+	 * @param v
+	 */
+	public void onClickShowFlowWindow(View v) {
+		Log.i(TAG, "onClickShowFlowWindow()");
+		SimulateKeyWinOpt.showFloatWindow(getApplicationContext());
+	}
+
+	/**
+	 * 隐藏悬浮窗
+	 * 
+	 * @param v
+	 */
+	public void onClickHiddenFlowWindow(View v) {
+		Log.i(TAG, "onClickHiddenFlowWindow()");
+		SimulateKeyWinOpt.hiddenFloatWindow(getApplicationContext());
+	}
+
+	/**
 	 * 悬浮窗按钮高度增加
 	 * 
 	 * @param v
 	 */
 	public void onClickAddHeight(View v) {
-		Intent intent = new Intent(
-				"tv.sanrenxing.awang.simulatekey.SimulateKeyService");
-		intent.putExtra(SimulateKeyService.KEY_HAS_EXTRA, true);
-		intent.putExtra(SimulateKeyService.KEY_DO_ACTION,
-				SimulateKeyService.DO_ACTION_ADDHEIGHT);
-		startService(intent);
+		Log.i(TAG, "onClickAddHeight()");
+		SimulateKeyWinOpt.btnAddHeight(getApplicationContext());
 	}
 
 	/**
@@ -85,12 +77,8 @@ public class MainActivity extends Activity {
 	 * @param v
 	 */
 	public void onClickMinusHeight(View v) {
-		Intent intent = new Intent(
-				"tv.sanrenxing.awang.simulatekey.SimulateKeyService");
-		intent.putExtra(SimulateKeyService.KEY_HAS_EXTRA, true);
-		intent.putExtra(SimulateKeyService.KEY_DO_ACTION,
-				SimulateKeyService.DO_ACTION_MINUSHEIGHT);
-		startService(intent);
+		Log.i(TAG, "onClickMinusHeight()");
+		SimulateKeyWinOpt.btnMinusHeight(getApplicationContext());
 	}
 
 	/**
@@ -99,6 +87,7 @@ public class MainActivity extends Activity {
 	 * @param v
 	 */
 	public void onClickSetButtonBg(View v) {
+		Log.i(TAG, "onClickSetButtonBg()");
 		// TODO : 实现取色盘 //
 		SimulateKeyUtils.showMessage(getApplicationContext(), "暂未实现");
 	}
@@ -109,6 +98,7 @@ public class MainActivity extends Activity {
 	 * @param v
 	 */
 	public void onClickSetTextColor(View v) {
+		Log.i(TAG, "onClickSetTextColor()");
 		// TODO : 实现取色盘 //
 		SimulateKeyUtils.showMessage(getApplicationContext(), "暂未实现");
 	}
---
+++ src/tv/sanrenxing/awang/simulatekey/SimulateKeyWinOpt.java
@@ -0,0 +1,68 @@
+package tv.sanrenxing.awang.simulatekey;
+
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * 模拟按键窗口操作类(其实可以改成AIDL的方式来和Service通讯，就不用这么多方法和步骤了)
+ * 
+ * @author aWang
+ * @since 2015-3-11
+ */
+public class SimulateKeyWinOpt {
+
+	/**
+	 * 显示悬浮窗
+	 * 
+	 * @param context
+	 */
+	public static void showFloatWindow(Context context) {
+		Intent intent = new Intent(SimulateKeyService.NAME);
+		// 第一次启动的时候，可以不带参数 //
+		intent.putExtra(SimulateKeyService.KEY_HAS_EXTRA, true);
+		intent.putExtra(SimulateKeyService.KEY_DO_ACTION,
+				SimulateKeyService.DO_ACTION_SHOWWINDOW);
+		context.startService(intent);
+	}
+
+	/**
+	 * 隐藏悬浮窗
+	 * 
+	 * @param context
+	 */
+	public static void hiddenFloatWindow(Context context) {
+		Intent intent = new Intent(SimulateKeyService.NAME);
+		intent.putExtra(SimulateKeyService.KEY_HAS_EXTRA, true);
+		intent.putExtra(SimulateKeyService.KEY_DO_ACTION,
+				SimulateKeyService.DO_ACTION_HIDDENWINDOW);
+		context.startService(intent);
+	}
+
+	/**
+	 * 悬浮窗按钮高度增加
+	 * 
+	 * @param context
+	 */
+	public static void btnAddHeight(Context context) {
+		Intent intent = new Intent(
+				"tv.sanrenxing.awang.simulatekey.SimulateKeyService");
+		intent.putExtra(SimulateKeyService.KEY_HAS_EXTRA, true);
+		intent.putExtra(SimulateKeyService.KEY_DO_ACTION,
+				SimulateKeyService.DO_ACTION_ADDHEIGHT);
+		context.startService(intent);
+	}
+
+	/**
+	 * 悬浮窗按钮高度减小
+	 * 
+	 * @param context
+	 */
+	public static void btnMinusHeight(Context context) {
+		Intent intent = new Intent(
+				"tv.sanrenxing.awang.simulatekey.SimulateKeyService");
+		intent.putExtra(SimulateKeyService.KEY_HAS_EXTRA, true);
+		intent.putExtra(SimulateKeyService.KEY_DO_ACTION,
+				SimulateKeyService.DO_ACTION_MINUSHEIGHT);
+		context.startService(intent);
+	}
+}
